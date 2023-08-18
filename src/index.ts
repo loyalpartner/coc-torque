b5b8d083ebe80b7f06ca4cee79c6370ffcc600d4
lee <loyalpartner@163.com>
2023-08-19 01:47:05 +0800

    fix: not work when first enter vim

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,47 @@
 import {
   ExtensionContext, LanguageClient,
-  LanguageClientOptions, workspace
+  LanguageClientOptions, ServerOptions, workspace
 } from 'coc.nvim'
+
+let client: LanguageClient
 
 export async function activate(context: ExtensionContext): Promise<void> {
 
-  const serverOptions = {
-    command: "torque-language-server"
+  const serverOptions: ServerOptions = {
+    command: 'torque-language-server',
+    options: {
+      cwd: workspace.rootPath
+    },
+    args: [ "-l" , "/tmp/test"]
+    
   }
 
   const clientOptions: LanguageClientOptions = {
-    documentSelector: [{scheme: 'file', language: 'torque'}],
-    initializationFailedHandler: () => false,
+    documentSelector: [{language: 'torque', pattern: '**/*.tq'}],
+    // initializationFailedHandler: () => false,
   }
 
-  const client = new LanguageClient(
+  client = new LanguageClient(
     'torque',
+    'torque-language-server',
     serverOptions, clientOptions
   )
 
 
   client.onDidChangeState(e => {})
-
   client.start()
 
-  // await workspace.loadFiles("**/*.tq")
-
   await client.onReady()
-
   workspace.findFiles("**/*.tq").then(urls => {
     client.sendNotification('torque/fileList', {
-      files: urls.map(t=>t.toString())
+      files: urls.map(t => t.toString())
     })
   })
 
   // context.subscriptions.push(services.registLanguageClient(client))
+}
+
+export function deactivate() {
+  if (!client) {return undefined;}
+  return client.stop();
 }
